16b8d61623dca68937827f9150882324c40b3175
Kevin Bowe <kevinbowe1999@gmail.com>
2020-01-26 00:30:23 -0500

    GitIgnore Added

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -2,50 +2,108 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class DashboardScreen extends StatelessWidget {
+  final meetings = <Courses>[];
+  void _addCourses({
+    DateTime start,
+    DateTime end,
+    String newClassNumber,
+    String newActivityName,
+    String newProfessor,
+    String newSection,
+  }) {
+    final Courses newCourse = Courses(
+        classNumber: newClassNumber,
+        activityName: newActivityName,
+        professor: newProfessor,
+        section: newSection,
+        from: start,
+        to: end,
+        background: const Color(0xFF0F8644),
+        isAllDay: false);
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           SfCalendar(
-            headerHeight: 40,
-            view: CalendarView.day,
-            dataSource: MeetingDataSource(_getDataSource()),
-            timeSlotViewSettings: TimeSlotViewSettings(
-              startHour: 8,
-              endHour: 22,
+              headerHeight: 40,
+              view: CalendarView.day,
+              dataSource: MeetingDataSource(_getDataSource(meetings)),
+              timeSlotViewSettings: TimeSlotViewSettings(
+                startHour: 8,
+                endHour: 22,
+              )
+            ),
+            Container(
+              width: double.infinity,
+              child: Row(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Course Name'),
+                    //controller: titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Course Number'),
+                    //controller: titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Professor'),
+                    //controller: titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Section'),
+                    //controller: titleController,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Start'),
+                        //controller: titleController,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'End'),
+                        //controller: titleController,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Day'),
+                        //controller: titleController,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Month'),
+                        //controller: titleController,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Year'),
+                        //controller: titleController,
+                      ),
+                    ],
+                  )
+                  
+
+                ],
+              ),
+            ),
+            RaisedButton(
+              child: Text('Add Class'),
+              onPressed: null ,
             )
-            
-          )
+
         ],
-       
       ),
     );
   }
-
-  
 }
 
- List<Courses> _getDataSource() {
-    final meetings = <Courses>[];
-    final DateTime today = DateTime.now();
-    final DateTime startTime =
-        DateTime(today.year, today.month, today.day, 9, 0, 0);
-    final DateTime endTime = startTime.add(const Duration(hours: 2));
-    meetings.add(
-        Courses(
-          classNumber: "MATH 201", 
-          activityName: "Operating Systems",
-          professor: "Aiman Hanna",
-          section: "AA",
-          from: startTime, 
-          to: endTime, 
-          background: const Color(0xFF0F8644), 
-          isAllDay: false)
-          );
-    return meetings;
-  }
-
+List<Courses> _getDataSource(List<Courses> meetings) {
+  return meetings;
+}
 
 class MeetingDataSource extends CalendarDataSource {
   MeetingDataSource(this.source);
@@ -70,15 +128,15 @@ class MeetingDataSource extends CalendarDataSource {
     return source[index].classNumber + ": \n" + source[index].activityName;
   }
 
-  String getClassNumber(int index){
+  String getClassNumber(int index) {
     return source[index].classNumber;
   }
 
-  String getProfessor(int index){
+  String getProfessor(int index) {
     return source[index].professor;
   }
 
-  String getSection(int index){
+  String getSection(int index) {
     return source[index].section;
   }
 
@@ -94,7 +152,15 @@ class MeetingDataSource extends CalendarDataSource {
 }
 
 class Courses {
-  Courses({this.activityName, this.classNumber, this.professor, this.section, this.from, this.to, this.background, this.isAllDay});
+  Courses(
+      {this.activityName,
+      this.classNumber,
+      this.professor,
+      this.section,
+      this.from,
+      this.to,
+      this.background,
+      this.isAllDay});
 
   String activityName;
   String classNumber;
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'login_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
